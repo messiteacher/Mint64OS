e7b9b33ded6ec035b4f1685f8d905f20ca631208
Shin Youn Ho <suh2963@gmail.com>
2023-05-20 02:12:52 +0900

    ch11

---
+++ ch11/02.Kernel64/Source/AssemblyUtility.h
@@ -0,0 +1,10 @@
+#ifndef __ASSEMBLYUTILITY_H__
+#define __ASSEMBLYUTILITY_H__
+
+#include "Types.h"
+
+// 함수
+BYTE kInProtByte(WORD wPort);
+void kOutPortByte(WORD wPort, BYTE bData);
+
+#endif /*__ASSEMBLYUTILITY_H__*/
