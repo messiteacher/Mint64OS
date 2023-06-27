1de6fd5cf309dd370cbe753cab4fe553ac170f5f
Shin Youn Ho <suh2963@gmail.com>
2023-06-27 16:28:57 +0900

    ch15

---
+++ ch15/02.Kernel64/Source/AssemblyUtility.h
@@ -0,0 +1,20 @@
+#ifndef __ASSEMBLYUTILITY_H__
+#define __ASSEMBLYUTILITY_H__
+
+#include "Types.h"
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  함수
+//
+////////////////////////////////////////////////////////////////////////////////
+BYTE kInPortByte( WORD wPort );
+void kOutPortByte( WORD wPort, BYTE bData );
+void kLoadGDTR( QWORD qwGDTRAddress );
+void kLoadTR( WORD wTSSSegmentOffset );
+void kLoadIDTR( QWORD qwIDTRAddress);
+void kEnableInterrupt(void);
+void kDisableInterrupt(void);
+QWORD kReadRFLAGS(void);
+
+#endif /*__ASSEMBLYUTILITY_H__*/
